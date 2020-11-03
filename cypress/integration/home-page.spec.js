3328061b7ceddeaf145cc7abea98734046ea2b36
Conor <conorbrett72@gmail.com>
2020-11-03 17:39:17 +0000

    E2E tests - Home page - Filtering by title - Exceptional case

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -66,6 +66,17 @@ describe("Home Page ", () => {
           .should("have.text", matchingMovies[index].title);
         })
       })
+      it("should display movies with 'xyz' in the title", () => {
+        const searchString = "xyz";
+        const matchingMovies = filterByTitle(movies, searchString);
+        cy.get("input").clear().type(searchString);
+        cy.get(".card").should("have.length", matchingMovies.length);
+        cy.get(".card").each(($card, index) => {
+          cy.wrap($card)
+          .find(".card-title")
+          .should("have.text", matchingMovies[index].title);
+        })
+      })
     })
     describe("By movie genre" ,() => {
       // More later
