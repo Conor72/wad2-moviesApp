700e03a60a8940c3b0c0cdcba5be3b331864e02f
Conor Brett <conorbrett72@gmail.com>
2020-11-29 01:35:37 +0000

    Login and Sign Up now creating users and will redirect to main page after account is created

--- src/components/auth/App.js
+++ src/components/auth/App.js
@@ -9,28 +9,58 @@ import Login from "./Login"
 import PrivateRoute from "./PrivateRoute"
 import ForgotPassword from "./ForgotPassword"
 import UpdateProfile from "./UpdateProfile"
+import MoviesContextProvider from "../../contexts/moviesContext";
+import GenresContextProvider from "../../contexts/genresContext";
+import SiteHeader from '../../components/siteHeader';
+import HomePage from "../../pages/homePage";
+import MoviePage from '../../pages/movieDetailsPage'
+import FavoriteMoviesPage from '../../pages/favoritesMoviesPage'       
+import UpcomingMoviesPage from '../../pages/upcomingMoviesPage'  // NEW
+import MovieReviewPage from "../../pages/addMovieReviewPage";
+import WatchListMoviesPage from "../../pages/watchListMoviesPage";
+import AddMovieReviewPage from '../../pages/addMovieReviewPage';
+import TopRatedMoviesPage from '../../pages/topRatedMoviesPage';
+import NowPlayingMoviesPage from '../../pages/nowPlayingMoviesPage';
+
 
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <Router>
+    <Router>
+    <div className="jumbotron">
+    <SiteHeader /> 
+    <div className="container-fluid">
+       
           <AuthProvider>
+          <MoviesContextProvider> 
+           <GenresContextProvider>  
             <Switch>
+            <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <PrivateRoute exact path="/movies/upcoming" component={UpcomingMoviesPage} />
               <PrivateRoute exact path="/" component={Dashboard} />
               <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
+              <PrivateRoute exact path="/reviews/form" component={AddMovieReviewPage} />
+              <PrivateRoute exact path="/movies/watchlist" component={WatchListMoviesPage} />
+              <PrivateRoute exact path="/movies/favorites" component={FavoriteMoviesPage} />
+              <PrivateRoute exact path="/movies/nowPlaying" component={NowPlayingMoviesPage} />
+              <PrivateRoute exact path="/movies/topRated" component={TopRatedMoviesPage} />
+              <PrivateRoute path="/reviews/:id" component={MovieReviewPage} />
+              <PrivateRoute path="/movies/:id" component={MoviePage} />
+              <PrivateRoute path="/" component={HomePage} />
+             
               <Route path="/forgot-password" component={ForgotPassword} />
             </Switch>
+            </GenresContextProvider>    
+        </MoviesContextProvider> 
           </AuthProvider>
-        </Router>
+       
+        </div>
       </div>
-    </Container>
-  )
-}
+      </Router>
+  );
+};
 
 export default App
+
+
+
