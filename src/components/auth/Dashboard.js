4137f017ac8ec1f94cfa7d7d85d415904a665753
Conor Brett <conorbrett72@gmail.com>
2020-12-10 18:57:08 +0000

    Firebase profile picture upload working on profile page and being stored persistantly

--- src/components/auth/Dashboard.js
+++ src/components/auth/Dashboard.js
@@ -4,6 +4,11 @@ import { Link, useHistory } from "react-router-dom"
 import React, { useState } from "react"
 import { Card, Button, Alert } from "react-bootstrap"
 
+import { render } from "react-dom";
+import { storage } from "./firebase.config";
+
+import "./styles.css";
+
 export default function Dashboard() {
   const [error, setError] = useState("")
   const { currentUser, logout } = useAuth()
@@ -19,6 +24,80 @@ export default function Dashboard() {
       setError("Failed to log out")
     }
   }
+
+
+
+  
+
+  
+  const ReactFirebaseFileUpload = () => {
+    const [image, setImage] = useState(null);
+    const [url, setUrl] = useState("");
+    const [progress, setProgress] = useState(0);
+  
+    const handleChange = e => {
+      if (e.target.files[0]) {
+        setImage(e.target.files[0]);
+      }
+    };
+  
+    const handleUpload = () => {
+      const uploadTask = storage.ref(`images/${image.name}`).put(image);
+      uploadTask.on(
+        "state_changed",
+        snapshot => {
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setProgress(progress);
+        },
+        error => {
+          console.log(error);
+        },
+        () => {
+          storage
+            .ref("images")
+            .child(image.name)
+            .getDownloadURL()
+            .then(url => {
+              setUrl(url);
+            });
+        }
+      );
+    };
+  
+    console.log("image: ", image);
+  
+    return (
+      <div>
+        <progress value={progress} max="100" />
+        <br />
+        <br />
+        <input type="file" onChange={handleChange} />
+        <button onClick={handleUpload}>Upload</button>
+        <br />
+        
+        <br />
+        <img class = "profilepicture" src={url || "http://via.placeholder.com/300"} alt="firebase-image" />
+        
+      </div>
+    );
+  };
+  
+  render(<ReactFirebaseFileUpload />, document.querySelector("#root"));
+
+
+
+
+
+  
+
+
+
+
+
+
+
 
   return (
     <>
