5f639e77ac3434d194db884065cd68a7007991e4
Conor Brett <conorbrett72@gmail.com>
2020-12-12 23:05:28 +0000

    Test: Fixed signup.spec.js to create a unique user each time it is tested

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,4 +1,6 @@
 let movies;    // List of movies from TMDB
+var val = Math.floor(1000 + Math.random() * 9000);      //Creates a random 4 digit number to ensure every user created for this test is unique
+
 
 /* eslint-disable */
   beforeEach(() => {
@@ -8,7 +10,7 @@ let movies;    // List of movies from TMDB
   
   describe("Signup", () => {
     it("Should successfully sign up user and change to a movies page", () => {
-      cy.get('#email').type('newaccount@gmail.com')
+      cy.get('#email').type('newaccount' + val + '@gmail.com')    //Adds the 4 digit value between newaccount and @gmail.com
       cy.get('#password').type('123456')
       cy.get('#password-confirm').type('123456')
       cy.get('.btn').click() 
