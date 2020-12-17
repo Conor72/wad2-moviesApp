4e6d0d2dcb1028ea40618e4a50d5e4632bb3b687
Conor Brett <conorbrett72@gmail.com>
2020-12-17 15:58:02 +0000

    Test: E2E - Testing Upcoming Movies page

---
+++ cypress/integration/upcomingMovies-page.spec.js
@@ -0,0 +1,43 @@
+let movies;    // List of movies from TMDB
+/* eslint-disable */
+// Utility functions
+const filterByTitle = (movieList, string) =>
+  movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+
+const filterByGenre = (movieList, genreId) =>
+  movieList.filter((m) => m.genre_ids.includes(genreId));
+
+describe("Popular Movies Page", () => {
+  before(() => {
+    // Get movies from TMDB and store in movies variable.
+    cy.request(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .its("body")    // Take the body of HTTP response from TMDB
+      .then((response) => {
+        movies = response.results
+      })
+  })
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/movies/upcoming")
+  });
+
+  describe("Base tests", () => {
+    
+  });
+  describe("Filtering", () => {
+    it("should only display movies with the specified title substring", () => {
+      let searchString = "m";
+      let matchingMovies = filterByTitle(movies, searchString);
+      cy.get("input").clear().type(searchString); // Enter m in text box
+      cy.get(".card").should("have.length", matchingMovies.length);
+      // Do a second test for certainty!
+      searchString = "i";
+      matchingMovies = filterByTitle(movies, searchString);
+      cy.get("input").clear().type(searchString)
+      cy.get(".card").should("have.length", matchingMovies.length);
+    });
+  });
+});
